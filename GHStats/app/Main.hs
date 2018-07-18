9c9511d70944df957343d81564bbe406279e20b0
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2018-07-19 01:46:56 +0200

    Add FromJSON instance for MergeableStatus

--- GHStats/app/Main.hs
+++ GHStats/app/Main.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE DeriveAnyClass     #-}
-{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+
+
 module Main where
 
 import qualified Data.ByteString.Char8 as L8
@@ -18,20 +21,31 @@ import qualified Data.HashMap.Strict as HM
 
 
 data PullRequest = PullRequest {
-                   title       :: Maybe Title
+                   title       :: Maybe Title    -- J-C are you sure Maybe is needed ? try with resp1 and see what happens
                  , isMerged    :: Maybe Bool
                  , isMergeable :: Maybe MergeableStatus
                  } deriving ( Show, Eq, Generic)
 
 data MergeableStatus = Mergeable | NotMergeable
- deriving (Show, Eq, Generic, FromJSON, ToJSON)
+ deriving (Show, Eq, Generic)
+
 
 type Title = T.Text
 
 samplePullReq = PullRequest (Just "someTitle") (Just True) (Just Mergeable)
 
-main = print "OK"
+main = do
+  let (d :: Either String PullRequest) = eitherDecode resp1
+  print d
 
+
+instance FromJSON MergeableStatus where
+ parseJSON = withText "MergeableStatus" $ \t ->
+  case t of
+    "MERGEABLE" -> return Mergeable
+    -- Check whether NOTMERGEABLE is a legal value
+    "NOTMERGEABLE" -> return NotMergeable
+    _ -> fail ("Bad Mergeable Status: " ++ T.unpack t)
 
 instance FromJSON PullRequest where
  parseJSON = parseResponse
@@ -59,6 +73,9 @@ parsePullrequest v = do
 --query response I'm trying to parse but not successful yet
 resp :: LL8.ByteString
 resp = "{\"data\":{\"organization\":{\"repository\":{\"pullRequests\":{\"edges\":[{\"node\":{\"title\":\"[CDEC-432] Switch `JsonLog` & `Mockable` imports from networking to core\",\"merged\":false,\"createdAt\":\"2018-07-18T14:18:59Z\",\"mergeable\":\"MERGEABLE\"}}]}}}}}\n"
+
+resp1 :: LL8.ByteString
+resp1 = "{\"data\":{\"organization\":{\"repository\":{\"pullRequests\":{\"edges\":[{\"node\":{\"title\":true,\"merged\":false,\"createdAt\":\"2018-07-18T14:18:59Z\",\"mergeable\":\"MERGEABLE\"}}]}}}}}\n"
 
 
 runQuery :: IO (LL8.ByteString)
