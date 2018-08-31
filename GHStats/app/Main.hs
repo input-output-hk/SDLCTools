bff418899dae6b8cbc02bd57925e145bd6d99300
deepakkapiswe <deepakkapiswe@gmail.com>
2018-08-31 17:49:57 +0530

    [CO-340] fixed compilation bug

--- GHStats/app/Main.hs
+++ GHStats/app/Main.hs
@@ -13,12 +13,38 @@ import           Data.List.Utils (replace)
 import qualified Data.Text as T
 import           Network.HTTP.Simple
 import           System.FilePath.Posix
+import           Options.Applicative
 
 import           Types
 import           Extract
 import           Report
-import           Misc
 
+
+data CliOptions = MkCliOptions {
+                  relPath  :: String
+                , apiToken :: String
+                , repoName :: String
+                } deriving (Show)
+
+optionParser :: Parser CliOptions
+optionParser =
+  MkCliOptions
+        <$> strOption (
+              long "relativePath"
+              <> short 'p'
+              <> (help "Relative Path to Query and Token Directory"))
+        <*> strOption (
+              long "Api Token"
+              <> short 't'
+              <> (help "GitHub APi Token"))
+        <*> strOption (
+              long "Repository Name"
+              <> short 'r'
+              <> (help "Name of Repository to query"))
+
+-- | Parse command line options
+parseCliArgs :: IO CliOptions
+parseCliArgs = customExecParser (prefs showHelpOnError) (info optionParser fullDesc)
 
 main :: IO ()
 main = do
