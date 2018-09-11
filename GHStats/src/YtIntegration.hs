1b5f2a15451053393ead089532c671f536d321bc
deepakkapiswe <deepakkapiswe@gmail.com>
2018-09-11 20:12:00 +0530

    [CO-340] Add YtIntegration file

---
+++ GHStats/src/YtIntegration.hs
@@ -0,0 +1,21 @@
+{-# Language BangPatterns         #-}
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+
+module YtIntegration where
+
+import qualified Data.Text as T
+import Types
+
+import Meas
+
+
+getYtInfo :: YtAuthorization -> Maybe YtIssueId -> IO (Maybe YtInfo)
+getYtInfo _ Nothing = return Nothing
+getYtInfo auth (Just issueId) = do
+  (tasks, issues) <- getSingleIssue auth (T.unpack issueId)
+  case (tasks, issues) of
+    ([task], [])  -> return . pure $ YtInfo TaskType  (_yttState task)
+    ([], [issue]) -> return . pure $ YtInfo IssueType (_ytiState issue)
+    _             -> error "should not happen"
