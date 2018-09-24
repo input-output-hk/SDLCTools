2203d47d2e77c45cb2b18140dd6047ea698f9dda
deepakkapiswe <deepakkapiswe@gmail.com>
2018-09-24 23:53:58 +0530

    [CO-340] Added more tables in schema

--- meas/sql/schema.sql
+++ meas/sql/schema.sql
@@ -4,45 +4,35 @@ DROP DATABASE IF EXISTS sdlc_db;
 CREATE DATABASE sdlc_db;
 \c sdlc_db;
 
-DROP TABLE IF EXISTS input_static_priorityValueDomain;
-DROP TABLE IF EXISTS input_static_iohksStateValueDomain;
-DROP TABLE IF EXISTS input_static_stateValueDomain;
-DROP TABLE IF EXISTS input_static_waitValueDomain;
-DROP TABLE IF EXISTS input_static_typeValueDomain;
-DROP TABLE IF EXISTS input_static_threeDValueDomain;
-DROP TABLE IF EXISTS input_static_romManDaysValueDomain;
-DROP TABLE IF EXISTS input_static_resolutionValueDomain;
-
-
-CREATE TABLE input_static_priorityValueDomain (
+CREATE TABLE priorityValueDomain (
   priorityValue text,
-  CONSTRAINT PKC_input_static_priorityValueDomain PRIMARY KEY (priorityValue)
+  CONSTRAINT PKC_priorityValueDomain PRIMARY KEY (priorityValue)
 );
 
-insert into input_static_priorityValueDomain values
+insert into priorityValueDomain values
     ('ShowStopper')
   , ('Critical')
   , ('Major')
   , ('Normal')
   , ('Minor');
 
-CREATE TABLE input_static_iohksStateValueDomain (
+CREATE TABLE iohksStateValueDomain (
   iohksStateValue text,
-  CONSTRAINT PKC_input_static_iohksStateValueDomain PRIMARY KEY (iohksStateValue)
+  CONSTRAINT PKC_iohksStateValueDomain PRIMARY KEY (iohksStateValue)
 );
 
-insert into input_static_iohksStateValueDomain values
+insert into iohksStateValueDomain values
     ('IohksSubmitted')
   , ('IohksReadyToSolve')
   , ('IohksFixed')
   , ('IohksDone');
 
-CREATE TABLE input_static_stateValueDomain (
+CREATE TABLE stateValueDomain (
   stateValue text,
-  CONSTRAINT PKC_input_static_stateValueDomain PRIMARY KEY (stateValue)
+  CONSTRAINT PKC_stateValueDomain PRIMARY KEY (stateValue)
 );
 
-insert into input_static_stateValueDomain values
+insert into stateValueDomain values
     ('Backlog')
   , ('Planning')
   , ('Selected')
@@ -50,66 +40,66 @@ insert into input_static_stateValueDomain values
   , ('Review')
   , ('Done');
 
-CREATE TABLE input_static_waitValueDomain (
+CREATE TABLE waitValueDomain (
   waitValue text,
-  CONSTRAINT PKC_input_static_waitValueDomain PRIMARY KEY (waitValue)
+  CONSTRAINT PKC_waitValueDomain PRIMARY KEY (waitValue)
 );
 
-insert into input_static_waitValueDomain values
+insert into waitValueDomain values
     ('Running')
   , ('Waiting');
 
 
-CREATE TABLE input_static_typeValueDomain (
+CREATE TABLE typeValueDomain (
   typeValue text,
-  CONSTRAINT PKC_input_static_typeValueDomain PRIMARY KEY (typeValue)
+  CONSTRAINT PKC_typeValueDomain PRIMARY KEY (typeValue)
 );
 
-insert into input_static_typeValueDomain values
+insert into typeValueDomain values
     ('TaskType')
   , ('IssueType')
   , ('OtherType');
 
 
-CREATE TABLE input_static_threeDValueDomain (
+CREATE TABLE threeDValueDomain (
   threeDValue text,
-  CONSTRAINT PKC_input_static_threeDValueDomain PRIMARY KEY (threeDValue)
+  CONSTRAINT PKC_threeDValueDomain PRIMARY KEY (threeDValue)
 );
 
-insert into input_static_threeDValueDomain values
+insert into threeDValueDomain values
     ('Design')
   , ('Development')
   , ('Documentation')
   , ('Test');
 
-CREATE TABLE input_static_romManDaysValueDomain (
+CREATE TABLE romManDaysValueDomain (
   romManDaysValue text,
-  CONSTRAINT PKC_input_static_romManDaysValueDomain PRIMARY KEY (romManDaysValue)
+  CONSTRAINT PKC_romManDaysValueDomain PRIMARY KEY (romManDaysValue)
 );
 
-insert into input_static_romManDaysValueDomain values
+insert into romManDaysValueDomain values
     ('Days')
   , ('Weeks')
   , ('Months')
   , ('Quarters');
 
-CREATE TABLE input_static_resolutionValueDomain (
+CREATE TABLE resolutionValueDomain (
   resolutionValue text,
-  CONSTRAINT PKC_input_static_resolutionValueDomain PRIMARY KEY (resolutionValue)
+  CONSTRAINT PKC_resolutionValueDomain PRIMARY KEY (resolutionValue)
 );
 
-insert into input_static_resolutionValueDomain values
+insert into resolutionValueDomain values
     ('Successful')
   , ('Aborted')
   , ('Duplicate')
   , ('Obsolete');
 
-CREATE TABLE input_static_linkTypeDomain (
+CREATE TABLE linkTypeDomain (
   linkType text,
-  CONSTRAINT PKC_input_static_linkTypeDomain PRIMARY KEY (linkType)
+  CONSTRAINT PKC_linkTypeDomain PRIMARY KEY (linkType)
 );
 
-insert into input_static_linkTypeDomain values
+insert into linkTypeDomain values
     ('ParentFor')
   , ('SubTaskOf')
   , ('MustStartAfter')
@@ -120,86 +110,184 @@ insert into input_static_linkTypeDomain values
   , ('IsDuplicatedBy')
   , ('IsRequiredFor');
 
-CREATE TABLE input_static_valueChangeTypeDomain (
+CREATE TABLE valueChangeTypeDomain (
   valueChangeType text,
-  CONSTRAINT PKC_input_static_valueChangeTypeDomain PRIMARY KEY (valueChangeType)
+  CONSTRAINT PKC_valueChangeTypeDomain PRIMARY KEY (valueChangeType)
 );
 
-insert into input_static_valueChangeTypeDomain values
+insert into valueChangeTypeDomain values
     ('Time Updated')
   , ('Updated By')
   , ('State Changed')
   , ('Wait Changed');
 
-CREATE TABLE input_dynamic_developers (
+CREATE TABLE developers (
   developerName text NOT NULL,
   otherDetails text,
-  CONSTRAINT PKC_input_dynamic_developers PRIMARY KEY (developerName)
+  CONSTRAINT PKC_developers PRIMARY KEY (developerName)
 );
 
-CREATE TABLE input_dynamic_squads (
+CREATE TABLE squads (
   squadId Integer NOT NULL,
   squadLead text NOT NULL,
   squadSize Integer NOT NULL,
-  CONSTRAINT PKC_input_dynamic_squads PRIMARY KEY (squadId),
-  FOREIGN KEY (squadLead) REFERENCES input_dynamic_developers (developerName)
+  CONSTRAINT PKC_squads PRIMARY KEY (squadId),
+  CONSTRAINT CHK_nonNegative CHECK  (squadId > 0 AND squadSize > 0),
+  FOREIGN KEY (squadLead) REFERENCES developers (developerName)
   ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE input_dynamic_squadDetails (
+CREATE TABLE squadDetails (
   squadId Integer NOT NULL,
   squadMember text NOT NULL,
-  CONSTRAINT PKC_input_dynamic_squadDetails PRIMARY KEY (squadId,squadMember),
-  FOREIGN KEY (squadId) REFERENCES input_dynamic_squads (squadId)
+  CONSTRAINT PKC_squadDetails PRIMARY KEY (squadId,squadMember),
+  CONSTRAINT CHK_nonNegative CHECK  (squadId > 0),
+  FOREIGN KEY (squadId) REFERENCES squads (squadId)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (squadMember) REFERENCES input_dynamic_developers (developerName)
+  FOREIGN KEY (squadMember) REFERENCES developers (developerName)
   ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE input_dynamic_targetVersionDomain (
-  targetVersion text NOT NULL,
-  CONSTRAINT PKC_input_dynamic_targetVersionDomain PRIMARY KEY (targetVersion)
+CREATE TABLE assigneeGroups (
+  assigneeGroupId Integer NOT NULL,
+  CONSTRAINT PKC_assigneeGroups PRIMARY KEY (assigneeGroupId)
 );
 
-CREATE TABLE aux_dynamic_targetVersionGroups (
-  targetVersionGroupId Integer NOT NULL,
-  CONSTRAINT PKC_input_dynamic_targetVersionGroups PRIMARY KEY (targetVersionGroupId)
-);
-
-CREATE TABLE input_dynamic_targetVersionGroupDetails (
-  targetVersionGroupId Integer NOT NULL,
-  targetVersion text NOT NULL,
-  CONSTRAINT PKC_input_dynamic_targetVersionGroupDetails PRIMARY KEY (targetVersionGroupId, targetVersion),
-  FOREIGN KEY (targetVersionGroupId) REFERENCES aux_dynamic_targetVersionGroups (targetVersionGroupId)
+CREATE TABLE assigneeGroupDetails (
+  assigneeGroupId Integer NOT NULL,
+  assignee text NOT NULL,
+  CONSTRAINT PKC_assigneeGroupDetails PRIMARY KEY (assigneeGroupId, assignee),
+  CONSTRAINT CHK_nonNegative CHECK  (assigneeGroupId >= 0),
+  FOREIGN KEY (assigneeGroupId) REFERENCES assigneeGroups (assigneeGroupId)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (targetVersion) REFERENCES input_dynamic_targetVersionDomain (targetVersion)
+  FOREIGN KEY (assignee) REFERENCES developers (developerName)
   ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE aux_dynamic_linkGroups (
+CREATE TABLE aux_ytGenericTickets (
+  ytgTicketId text NOT NULL,
+  typeValue text NOT NULL,
+  CONSTRAINT PKC_aux_ytgTicketId PRIMARY KEY (ytgTicketId)
+);
+
+CREATE TABLE targetVersionDomain (
+  targetVersion text NOT NULL,
+  CONSTRAINT PKC_targetVersionDomain PRIMARY KEY (targetVersion)
+);
+
+CREATE TABLE aux_targetVersionGroups (
+  targetVersionGroupId Integer NOT NULL,
+  CONSTRAINT PKC_targetVersionGroups PRIMARY KEY (targetVersionGroupId),
+  CONSTRAINT CHK_nonNegative CHECK (targetVersionGroupId >= 0)
+);
+
+CREATE TABLE targetVersionGroupDetails (
+  targetVersionGroupId Integer NOT NULL,
+  targetVersion text NOT NULL,
+  CONSTRAINT PKC_targetVersionGroupDetails PRIMARY KEY (targetVersionGroupId, targetVersion),
+  CONSTRAINT CHK_nonNegative CHECK  (targetVersionGroupId >= 0),
+  FOREIGN KEY (targetVersionGroupId) REFERENCES aux_targetVersionGroups (targetVersionGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (targetVersion) REFERENCES targetVersionDomain (targetVersion)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE aux_linkGroups (
   linkGroupId Integer NOT NULL,
-  CONSTRAINT PKC_input_dynamic_linkGroups PRIMARY KEY (linkGroupId)
+  CONSTRAINT PKC_aux_linkGroups PRIMARY KEY (linkGroupId),
+  CONSTRAINT CHK_nonNegative CHECK  (linkGroupId >= 0)
 );
 
-CREATE TABLE input_dynamic_linkGroupDetails (
+CREATE TABLE linkGroupDetails (
   linkGroupId Integer NOT NULL,
   linkType text NOT NULL,
   linkedTicketId text NOT NULL,
-  CONSTRAINT PKC_input_dynamic_linkGroupDetails PRIMARY KEY (linkGroupId,linkType,linkedTicketId),
-  FOREIGN KEY (linkGroupId) REFERENCES aux_dynamic_linkGroups (linkGroupId)
+  CONSTRAINT PKC_linkGroupDetails PRIMARY KEY (linkGroupId, linkType, linkedTicketId),
+  CONSTRAINT CHK_nonNegative CHECK  (linkGroupId >= 0),
+  FOREIGN KEY (linkGroupId) REFERENCES aux_linkGroups (linkGroupId)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (linkType) REFERENCES input_static_linkTypeDomain (linkType)
+  FOREIGN KEY (linkType) REFERENCES linkTypeDomain (linkType)
   ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE input_dynamic_ytgErrors (
-  ytgTicketId text NOT NULL,
-  ytError     text NOT NULL,
-  CONSTRAINT PKC_input_dynamic_ytgErrors PRIMARY KEY (ytgTicketId,ytError)
-  -- later to add FK 
+CREATE TABLE aux_changeGroups (
+  changeGroupId Integer NOT NULL,
+  CONSTRAINT PKC_aux_changeGroups PRIMARY KEY (changeGroupId),
+  CONSTRAINT CHK_nonNegative CHECK  (changeGroupId >= 0)
 );
 
-CREATE TABLE input_dynamic_ytIssueDetails (
+CREATE TABLE Changes (
+  changeId Integer NOT NULL,
+  changeGroupId Integer NOT NULL,
+  updateTime Integer NOT NULL,
+  updater text,
+  oldStateVal text,
+  newStateVal text,
+  oldWaitVal text,
+  newWaitVal text,
+  CONSTRAINT PKC_changeId PRIMARY KEY (changeId),
+  CONSTRAINT CHK_nonNegative CHECK  (changeId >= 0 AND changeGroupId >= 0 AND updateTime >= 0),
+  FOREIGN KEY (changeGroupId) REFERENCES aux_changeGroups (changeGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (oldStateVal) REFERENCES stateValueDomain (stateValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (newStateVal) REFERENCES stateValueDomain (stateValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (oldWaitVal) REFERENCES waitValueDomain (waitValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (newWaitVal) REFERENCES waitValueDomain (waitValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE stateTransitionValueDomain (
+  stateTransitionValue text NOT NULL,
+  CONSTRAINT PKC_stateTransitionValue PRIMARY KEY (stateTransitionValue)
+);
+
+insert into stateTransitionValueDomain values
+    ('STBacklog')
+  , ('STSelected')
+  , ('STInProgress')
+  , ('STInReview')
+  , ('STDone')
+  , ('STIllegalStateTransitions');
+
+
+CREATE TABLE stateTransitions (
+  stateTransitionId Integer NOT NULL,
+  ytgTicketId text NOT NULL,
+  stateTransitionValue text NOT NULL,
+  backlogTime Integer,
+  selectedTime Integer,
+  progressStartTime Integer,
+  reviewStartTime Integer,
+  doneTime Integer,
+  CONSTRAINT PKC_stateTransitions PRIMARY KEY (stateTransitionId),
+  CONSTRAINT CHK_nonNegative CHECK (stateTransitionId >= 0 AND backlogTime >= 0 AND
+  selectedTime >= 0 AND progressStartTime >= 0 AND reviewStartTime >= 0 AND doneTime >= 0),
+  FOREIGN KEY (ytgTicketId) REFERENCES aux_ytGenericTickets (ytgTicketId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (stateTransitionValue) REFERENCES stateTransitionValueDomain (stateTransitionValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE ytErrorGroups (
+  ytErrorGroupId Integer NOT NULL,
+  CONSTRAINT PKC_ytgErrorGroups PRIMARY KEY (ytErrorGroupId),
+  CONSTRAINT CHK_nonNegative CHECK (ytErrorGroupId >= 0)
+);
+
+CREATE TABLE ytErrorDetails (
+  errorId Integer NOT NULL,
+  ytErrorGroupId Integer NOT NULL,
+  ytError text NOT NULL,
+  CONSTRAINT PKC_ytErrorDetails PRIMARY KEY (errorId),
+  CONSTRAINT CHK_nonNegative CHECK (errorId >= 0 AND ytErrorGroupId >= 0 ),
+  FOREIGN KEY (ytErrorGroupId) REFERENCES ytErrorGroups (ytErrorGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE ytIssueDetails (
   ytiIssueId text NOT NULL,
   ytiType    text NOT NULL,
   ytiSummary text NOT NULL,
@@ -217,26 +305,79 @@ CREATE TABLE input_dynamic_ytIssueDetails (
   ytiPriority3 Integer NOT NULL,
   ytiSquadId Integer,
   ytiTargetVersionGroupId Integer,
-  ytiowner   text,
+  ytiOwner   text,
   ytiResolution text NOT NULL,
   ytiLinkGroupId Integer,
-  ytiStateTransitionId Integer NOT NULL,     -- todo : to add tables related to this
+  ytiChangeGroupId Integer,
+  ytiStateTransitionId Integer NOT NULL,
   ytiBlockedDays Integer NOT NULL,
-  CONSTRAINT PKC_input_dynamic_YtIssueDetails PRIMARY KEY (ytiIssueId),
-  FOREIGN KEY (ytiType) REFERENCES input_static_typeValueDomain (typeValue)
+  ytiErrorGroupId Integer NOT NULL,
+  CONSTRAINT PKC_YtIssueDetails PRIMARY KEY (ytiIssueId),
+  CONSTRAINT CHK_nonNegative CHECK (ytiCreated >= 0 AND ytiUpdatedAt >= 0
+  AND ytiNumber >= 0 AND ytiDueDate >= 0 AND ytiPriority1 >= 0 AND ytiPriority2 >= 0
+  AND ytiPriority3 >= 0 AND ytiSquadId >=0 AND ytiTargetVersionGroupId >=0
+  AND ytiLinkGroupId >=0 AND ytiChangeGroupId >=0 AND ytiStateTransitionId >=0
+  AND ytiBlockedDays >=0 AND ytiErrorGroupId >=0),
+  FOREIGN KEY (ytiType) REFERENCES typeValueDomain (typeValue)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (ytiState) REFERENCES input_static_stateValueDomain (stateValue)
+  FOREIGN KEY (ytiState) REFERENCES stateValueDomain (stateValue)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (ytiWait) REFERENCES input_static_waitValueDomain (waitValue)
+  FOREIGN KEY (ytiWait) REFERENCES waitValueDomain (waitValue)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (ytiROMManDay) REFERENCES input_static_romManDaysValueDomain (romManDaysValue)
+  FOREIGN KEY (ytiROMManDay) REFERENCES romManDaysValueDomain (romManDaysValue)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (ytiResolution) REFERENCES input_static_resolutionValueDomain (resolutionValue)
+  FOREIGN KEY (ytiResolution) REFERENCES resolutionValueDomain (resolutionValue)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (ytiSquadId) REFERENCES input_dynamic_squads (squadId)
+  FOREIGN KEY (ytiSquadId) REFERENCES squads (squadId)
   ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (ytiTargetVersionGroupId) REFERENCES aux_dynamic_targetVersionGroups (targetVersionGroupId)
+  FOREIGN KEY (ytiTargetVersionGroupId) REFERENCES aux_targetVersionGroups (targetVersionGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (ytiLinkGroupId) REFERENCES aux_linkGroups (linkGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (ytiChangeGroupId) REFERENCES aux_changeGroups (changeGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (ytiStateTransitionId) REFERENCES stateTransitions (stateTransitionId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (ytiErrorGroupId) REFERENCES ytErrorGroups (ytErrorGroupId)
   ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
+CREATE TABLE ytTaskDetails (
+  yttTaskId text NOT NULL,
+  yttSummary text NOT NULL,
+  yttDescription text NOT NULL,
+  yttCreated Integer NOT NULL,
+  yttUpdatedAt Integer,
+  yttProject text NOT NULL,
+  yttNumber  Integer NOT NULL,
+  yttState   text NOT NULL,
+  yttWait    text NOT NULL,
+  yttThreeDValue text NOT NULL,
+  yttAssigneeGroupId Integer,
+  yttLinkGroupId Integer,
+  yttChangeGroupId Integer,
+  yttStateTransitionId Integer NOT NULL,
+  yttBlockedDays Integer NOT NULL,
+  yttParent text NOT NULL,
+  yttErrorGroupId Integer NOT NULL,
+  CONSTRAINT PKC_YtTaskDetails PRIMARY KEY (yttTaskId),
+  CONSTRAINT CHK_nonNegative CHECK (yttCreated >= 0 AND yttUpdatedAt >= 0
+  AND yttNumber >= 0 AND yttAssigneeGroupId >= 0 AND yttLinkGroupId >= 0
+  AND yttChangeGroupId >=0 AND yttStateTransitionId >=0 AND yttBlockedDays >=0
+  AND yttErrorGroupId >=0),
+  FOREIGN KEY (yttState) REFERENCES stateValueDomain (stateValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (yttWait) REFERENCES waitValueDomain (waitValue)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (yttAssigneeGroupId) REFERENCES assigneeGroups (assigneeGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (yttLinkGroupId) REFERENCES aux_linkGroups (linkGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (yttChangeGroupId) REFERENCES aux_changeGroups (changeGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (yttStateTransitionId) REFERENCES stateTransitions (stateTransitionId)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (yttErrorGroupId) REFERENCES ytErrorGroups (ytErrorGroupId)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+);
