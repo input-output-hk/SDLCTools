7f959ca185efd2e1f55ce23758290fa864bc72eb
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2018-11-11 13:30:06 +0100

    Add sql drop tables

---
+++ meas/sql/drop.sql
@@ -0,0 +1,52 @@
+
+
+
+drop table links;
+drop table issueStateChanges;
+drop table issueWaitChanges;
+drop table targetVersion;
+drop table TaskAssignee;
+
+drop table ytIssueDetails;
+drop table ytTaskDetails;
+drop table tickets;
+
+drop table stateTransitions;
+
+
+drop table priorityDomain;
+drop table stateDomain;
+drop table waitDomain;
+drop table typeDomain;
+drop table threeDDomain;
+drop table romManDaysDomain;
+drop table resolutionDomain;
+drop table linkTypeDomain;
+drop table valueChangeTypeDomain;
+drop table stateTransitionDomain;
+
+drop table yttpLinks;
+drop table yttpBrowserAndVersions;
+drop table yttpTargetOS;
+drop table yttpTestingType;
+drop table yttpAssignee;
+drop table yttpPassedVersions;
+drop table yttpFailedVersions;
+drop table yttpBlockedVersions;
+drop table yttpCoveredComponents;
+
+drop table yttpIssueDetails;
+
+
+
+drop table yttpTypeDomain;
+drop table yttpStateDomain;
+drop table yttpPriorityDomain;
+drop table yttpReviewStatusDomain;
+drop table yttpAutomationStatusDomain;
+drop table yttpTestResultDomain;
+drop table yttpTargetOSDomain;
+drop table yttpTestingTypeDomain;
+drop table yttpBrowserVersionDomain;
+
+drop table developers;
