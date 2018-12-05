056dfee49e153acf87607360ce3d639b42505160
deepakkapiswe <deepakkapiswe@gmail.com>
2018-12-05 17:28:03 +0530

    Add DevNames.hs

---
+++ cycletimes/src/GH/DevNames.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE BangPatterns         #-}
+
+module GH.DevNames where
+
+import qualified  Data.Text as T
+import qualified  Data.Map.Strict as M
+
+devMap :: M.Map T.Text T.Text
+devMap = M.fromList
+  [ ("Anviking", "Johannes Lund")
+  , ("JaredCorduan", "Jared Corduan")
+  , ("KtorZ", "Matthias Benkort")
+  , ("denisshevchenko", "Denis Shevchenko")
+  , ("dnadales", "Damian Nadales")
+  , ("mdimjasevic", "Marko Dimjasevic")
+  , ("nc6", "Nicholas Clarke")
+  , ("parsonsmatt", "Matt Parsons")
+  , ("paweljakubas", "Pawel Jakubas")
+  , ("polinavino", "Polina Vinogradova")
+  , ("uroboros", "Ryan Lemmer")
+  , ("redxaxder", "redxaxder")
+  , ("ruhatch", "Rupert Horlick")
+  , ("Jimbo4350", "Jordan Millar")
+  , ("kantp", "Philipp Kant")
+  , ("mgudemann", "Matthias Güdemann")
+  , ("ksaric", "Kristijan Šarić")
+  , ("HirotoShioi", "Hiroto Shioi")
+  , ("robcohen", "Rob Cohen")
+  ]
+
+toRealName :: T.Text -> T.Text
+toRealName psuedo = case M.lookup psuedo devMap of
+  Nothing -> psuedo
+  Just uname -> uname
